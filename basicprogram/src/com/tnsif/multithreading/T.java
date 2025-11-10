b38c1d39e31ede45d1df1dab8ac8d387d49d0ff2
Hajira <hajira1726@gmail.com>
2025-11-10 19:56:35 +0530

    Initial commit

--- basicprogram/src/com/tnsif/multithreading/T.java
+++ basicprogram/src/com/tnsif/multithreading/T.java
@@ -1,5 +1,12 @@
 package com.tnsif.multithreading;
 
-public class T {
+public class T extends Thread{
+//	Example e;
+//	T(Example e){
+//		this.e=e;
+//	}
+	public void run() {
+		Example.display();
+	}
 
 }
